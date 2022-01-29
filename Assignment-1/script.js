0b16667d07370105037af01f58f44ea85c841525
Manjot singh <w0779181@myscc.ca>
2022-01-29 10:13:10 -0800

    uppdate

---
+++ Assignment-1/script.js
@@ -0,0 +1,13 @@
+/*
+	WEB 303 Assignment 1 - jQuery
+	{Manjot Singh}
+*/
+$(document).ready(function(){
+	$("label").keyup(function(){
+		const P = $("#percent").val();
+	const Ysalary = $("#yearly-salary").val();
+	const spendingAmount = (Ysalary * P) / 100;
+	$("#amount").text("$" + spendingAmount.toFixed(2));
+  });
+ 
+});
