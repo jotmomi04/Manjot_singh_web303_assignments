4700dac2eb4506565f317b63c37df700b9dee520
Manjot singh <w0779181@myscc.ca>
2022-02-13 15:33:16 -0800

    Update script.js

--- Assignment-3/js/script.js
+++ Assignment-3/js/script.js
@@ -11,30 +11,31 @@
 
 });
 console.log("the team :", team);*/
-$(function teamB() {
-    $.ajax({
-      url: "team.json",
-      beforeSend: function () {
-        let loadText = $("<h2></h2>").text("Loading...");
-        $(`#team`).append(loadText);
-      },
+$(function usingAjax() {
+	$.ajax({url: "team.json", 
+	beforeSend: function() {
+		var beforeText = $("<p></p>").text("Loading...");
+		$(`div#team`).append(beforeText);
+	},
+	error: function(){
+		
+	},
+	timeout: 5000,
+
+	
+	success: function(data){
+		$(`div#team`).empty();
+        $.each(data.members,function(a,b){
+            console.log("data a:",a,"and data b:",b);
       
-      timeout: 3000,
-  
-      success: function (data) {
-        $(`#team`).empty();
-        $.each(data.members, function (a, b) {
-          console.log("data a:", a, "and data b:", b);
-  
-          $("#team").append(
-            `
-              <h2>${b.name}</h2>
-              <h5>${b.position}</h5>
-              <p>${b.bio}</p>
-              `
-          );
-        });
-      }
-    });
-  });
-  
+            $('#team').append(`
+            <h2>${b.name}</h2>
+            <h5>${b.position}</h5>
+            <p>${b.bio}</p>
+            `);
+        })  
+	},timeout
+});
+});
+
+
