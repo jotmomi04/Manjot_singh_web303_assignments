7355985d2650a248fc5810c0cbd5e7bc00615568
Manjot singh <w0779181@myscc.ca>
2022-02-13 13:45:54 -0800

    Update script.js

--- Assignment-3/js/script.js
+++ Assignment-3/js/script.js
@@ -1,4 +1,4 @@
-/*let team = $.getJSON("team.json").done(function(data){
+let team = $.getJSON("team.json").done(function(data){
   $.each(data.members,function(a,b){
       console.log("data a:",a,"and data b:",b);
 
@@ -10,12 +10,28 @@
   })  ;
 
 });
-console.log("the team :", team);*/
+console.log("the team :", team);
 
-$(function teamB() {
+
 	$.ajax({url: "team.json", 
 	sendBefore:function() {
-		$('#team').append('<h2>Loading...</h2>').show(3000)
-	}
-})
-})
+		$('#team').append('<h2>Loading...</h2>')
+	},
+	
+	success:function(data){
+		$(`div#team`).empty();
+        $.each(data.members,function(a,b){
+            console.log("data a:",a,"and data b:",b);
+      
+            $('#team').append(`
+            <h2>${b.name}</h2>
+            <h5>${b.position}</h5>
+            <p>${b.bio}</p>
+            `);
+        });
+    }
+});
+$(document).ready(function () {
+    $.getJSON();
+  });
+
