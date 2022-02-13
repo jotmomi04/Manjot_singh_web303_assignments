b7f8add86285f95485ba0cb692ad39a5f1ff2d2a
Manjot singh <w0779181@myscc.ca>
2022-02-13 15:19:49 -0800

    Update script.js

--- Assignment-3/js/script.js
+++ Assignment-3/js/script.js
@@ -12,24 +12,29 @@
 });
 console.log("the team :", team);*/
 $(function teamB() {
-	$.ajax({url: "team.json", 
-	sendBefore:function() {
-		$('#team').append('<h2>Loading...</h2>').show()
-	},
-    timeout:3000,	
-	success: function(data){
-		$(`div#team`).empty();
-        $.each(data.members,function(a,b){
-            console.log("data a:",a,"and data b:",b);
+    $.ajax({
+      url: "team.json",
+      beforeSend: function () {
+        let beforeText = $("<h2></h2>").text("Loading...");
+        $(`#team`).append(beforeText);
+      },
       
-            $('#team').append(`
-            <h2>${b.name}</h2>
-            <h5>${b.position}</h5>
-            <p>${b.bio}</p>
-            `);
-        })  
-	},timeout:3000
-});
-});
-
-
+      timeout: 3000,
+  
+      success: function (data) {
+        $(`#team`).empty();
+        $.each(data.members, function (a, b) {
+          console.log("data a:", a, "and data b:", b);
+  
+          $("#team").append(
+            `
+              <h2>${b.name}</h2>
+              <h5>${b.position}</h5>
+              <p>${b.bio}</p>
+              `
+          );
+        });
+      }
+    });
+  });
+  
