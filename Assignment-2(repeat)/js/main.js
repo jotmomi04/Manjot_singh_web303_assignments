4a6710d1296caa905bf91c252b404c4b52d06aa1
Manjot singh <w0779181@myscc.ca>
2022-02-15 19:07:10 -0800

    Update main.js

--- Assignment-2(repeat)/js/main.js
+++ Assignment-2(repeat)/js/main.js
@@ -1,3 +1,46 @@
-// WEB303 Assignment 2
-
-
+$(document).ready(function () {
+    $("#prospect").on("click", function () {
+      let xhr = new XMLHttpRequest();
+  
+      xhr.open("GET", "prospect.html", true);
+  
+      xhr.onload = function () {
+        if (xhr.status == 200) {
+          let getProspect = document.getElementById("content");
+          getProspect.style.display = "block";
+          getProspect.innerHTML = xhr.responseText;
+        }
+      };
+      xhr.send();
+    });
+    $("#convert").on("click", function () {
+        let xhr = new XMLHttpRequest();
+    
+        xhr.open("GET", "convert.html", true);
+    
+        xhr.onload = function () {
+          if (xhr.status == 200) {
+            let getConvert = document.getElementById("content");
+            getConvert.style.display = "block";
+            getConvert.innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send();
+      });
+      $("#retain").on("click", function () {
+        let xhr = new XMLHttpRequest();
+    
+        xhr.open("GET", "retain.html", true);
+    
+        xhr.onload = function () {
+          if (xhr.status == 200) {
+            let getRetain = document.getElementById("content");
+            getRetain.style.display = "block";
+            getRetain.innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send();
+      });
+  
+    
+  });
