de117a55a8b087d40536b89f7d9f9a375af5f7eb
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-04-17 06:56:03 -0400

    update validation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,71 @@
-(function() {
-    let form = document.getElementsById('myform');
-    let password = document.getElementById('pwd');
-    let confirmPassword = document.getElementById('confirmpwd');
-    let submit = document.getElementById('submit');
+    let form = document.getElementById('myform'); // get  form
+    let password = document.getElementById('pwd'); // get password input
+    let submit = document.getElementById('submit'); // get submit button
+    let username = document.getElementById('username'); //get username
+    let cpassword = document.getElementById('confirmPwd'); // get password input
+    let list = document.getElementById('list'); // get country list
+    let term = document.getElementById('term').checked; // get term checkbox input
 
-    let submitted = false;
-    submit.disabled = true;
-    submit.className = 'disabled';
-    console.log(submit.className);
-})
+    form.addEventListener('submit', function(e) {
+
+        e.preventDefault();
+        validateInputs();
+    });
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success')
+
+    }
+    const setSuccess = (element) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error')
+
+
+    }
+
+    var validateInputs = () => {
+        const usernamevalue = username.value.trim() // use trim to avoid whitespace
+        const passwordvalue = password.value.trim();
+        const cpasswordvalue = cpassword.value.trim();
+        const termvalue = term;
+        //const termvalue = term.value();
+
+        if (usernamevalue === '') {
+            setError(username, 'Username is requierd');
+        } else {
+            setSuccess(username);
+        }
+
+        if (passwordvalue === '') {
+            setError(password, 'password is required');
+
+        } else if (passwordvalue.length < 12) {
+            setError(password, 'Password must be 12 charcter')
+
+        } else {
+            setSuccess(password);
+        }
+
+        if (cpasswordvalue === '') {
+            setError(cpassword, 'Confirm your password')
+        } else if (cpasswordvalue !== passwordvalue) {
+            setError(cpassword, 'password not matched')
+        } else {
+            setSuccess(cpassword);
+        }
+
+        if (termvalue == false) {
+            setError(term, 'please check the box')
+        } else {
+            setSuccess(term);
+        }
+
+    }
